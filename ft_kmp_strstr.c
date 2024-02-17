05037f8e73bbcb789c9655832e9babc70079ffc8
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-02-17 23:06:58 +0900

    add skip_list_create function

--- ft_kmp_strstr.c
+++ ft_kmp_strstr.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <strlib.h>
+#include <stdlib.h>
 
 /*
 * # ft_strlen
@@ -10,6 +10,7 @@ unsigned int	ft_strlen(char *str)
 {
 	unsigned int count;
 
+	count = 0;
 	while (*str)
 	{
 		count++;
@@ -23,6 +24,7 @@ unsigned int	ft_strlen(char *str)
 * 
 * return first matching pointer will return
 */
+/*
 int	*ft_strstr_test(char *str,char *target)
 {
 	// kmp algorithum
@@ -31,12 +33,25 @@ int	*ft_strstr_test(char *str,char *target)
 	skip_list = create_skip_list(str);
 	return (skip_list);
 }
-
-/*
-* int 
 */
-int	ft_streq()
+
+
+int	ft_streq(char *str0,char *str1,int stop_index)
 {
+	int i;
+
+	i = 0;
+	while (*str0 && i < stop_index)
+	{
+		if (*str0 != *str1)
+		{
+			return (0);
+		}
+		str0++;
+		str1++;
+		i++;
+	}
+	return (1);
 }
 
 /*
@@ -46,25 +61,31 @@ int	ft_streq()
 */
 int	*create_skip_list(char *str)
 {
-	char *str_tmp;
 	int *skip_list;
 	int str_length;
 	int i;
 	int slide;
 
-	str_tmp = str;
 	str_length = ft_strlen(str);
-	skip_list = malloc(sizeof(int) * str_length);
+	skip_list = malloc(sizeof(int) * str_length);// list for return
 	if (!skip_list)
 		return (NULL);
 	i = 0;
 	skip_list[i] = 1;
 	i++;
-	while (i < length)
+	while (i < str_length)
 	{
+		// i文字目で失敗したとき
+		// どれくらい文字をずらせばいいのかを
+		// 計算する
 		slide = 1;
-		
-		skip_list[i] = ;
+		printf("%d %s %s %d\n",i,&(str[slide]),str, i-slide);
+		while (slide < i && !ft_streq(&(str[slide]),str,i-slide))
+		{
+			slide++;
+			printf("%d %s %s %d\n",i,&(str[slide]),str, i-slide);
+		}
+		skip_list[i] = slide;
 		i++;
 	}
 	return (skip_list);
@@ -75,10 +96,11 @@ int	main(void)
 {
 	char *a = "abababbabcabba";
 	char *target = "abcabba";
-	int string_length = ft_strlen(target);
-	int *skip_list = create_skip_list(a);
-	
-	for (int i = 0; i<string_length; i++)
+ 	unsigned int string_length = ft_strlen(target);
+	int *skip_list = create_skip_list(target);
+
+	printf("length of target %d\n", string_length);	
+	for (unsigned int i = 0; i < string_length; i++)
 	{
 		printf("%d\n",skip_list[i]);
 	}
